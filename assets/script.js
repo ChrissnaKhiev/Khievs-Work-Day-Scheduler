431e7fc860846713239777d216e7729765004ccf
Chris Khiev <chrissnakhiev@gmail.com>
2022-09-23 23:19:42 -0400

    broken local storage save

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
 var currentDay = moment().format('dddd MMMM Do');
 $('#currentDay').text(currentDay);
+
 var hours = document.querySelectorAll('.row');
 var currHour =  moment().hours();
+var texts = document.querySelectorAll('.description');
+var currentList = 0;
+
+var tArea = document.querySelectorAll('.row');
+var todo = "";
 
 function logTime(){
         hours.forEach(hour => { // grabs each row and runs the function compareTime for each
@@ -21,4 +27,25 @@ function compareTime(element){ // for each hour this function compares to the cu
         element.childNodes[3].classList.add('future');
 }
 
+function logText(){
+    hours.forEach(text => {
+        saveToDo(text);
+    });
+}
+
+function saveToDo(element){
+    var text = element.childNodes[3].value;
+    localStorage.setItem(todo, text.value);
+    console.log(element.childNodes[3].value);
+}
+
+function loadText(){
+    var textL = localStorage.getItem(todo);
+    hours.childNodes[3].value = textL;
+};
+
 logTime();
+console.log(document.querySelector('.row').childNodes);
+hours.forEach(text => {
+    loadText(text);
+});
