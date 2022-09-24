fea567f495fb11ff27b2aabc9769d44dd0b446e4
Chris Khiev <chrissnakhiev@gmail.com>
2022-09-23 20:32:48 -0400

    starting function for time check

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,20 @@
 var currentDay = moment().format('dddd MMMM Do');
 $('#currentDay').text(currentDay);
+var hours = document.querySelectorAll('.hour');
+var currHour =  moment().hour();
+
+
+function checkTime(){
+    
+    if (hours < currHour){
+        document.getElementById("hour").classList.add('past');
+        document.getElementById("hour").classList.remove('hour');
+    } else if (hours = currHour){
+        document.getElementById("hour").classList.add('present');
+        document.getElementById("hour").classList.remove('hour');
+    } else 
+        document.getElementById("hour").classList.add('future');
+        document.getElementById("hour").classList.remove('hour');
+}
+
+document.addEventListener("load", checkTime);
